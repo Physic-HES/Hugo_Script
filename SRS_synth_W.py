3b347cbc94ede104c4e91a36118157389c93dd45
Hugo Notbook CNEA <huguito.ernest@gmail.com>
2020-09-29 07:14:48 -0300

    vectorizacion de iteraciones, reemplazo de sentencias mas robustas, mejora de convergencia, (conserva comentarios de version anterior)

--- SRS_synth_W.py
+++ SRS_synth_W.py
@@ -4,15 +4,14 @@ import matplotlib.pyplot as plt
 from scipy import linalg as npl
 
 # Sintesis de señal
-def sint(P):
-    w=np.zeros(np.int(np.floor(Fs*0.3)))
+def sint(P,tim):
+    t1=np.arange(0,tim,1/Fs)
+    w = np.zeros(len(t1))
     for j in range(len(P[0,:])-1):
         t=np.arange(P[1,j],P[1,j]+P[3,j]/(2*P[0,j]),1/Fs)
         w2=P[2,j]*np.sin(2*np.pi*P[0,j]/P[3,j]*(t-P[1,j]))*np.sin(2*np.pi*P[0,j]*(t-P[1,j]))
-        k=np.int(np.floor(P[1,j]*Fs))
-        if len(w)-(k+len(w2))>0:
-            w+=np.concatenate((np.zeros(k),w2,np.zeros(len(w)-(k+len(w2)))))
-    t1=np.linspace(0,0.3,len(w))
+        k=np.argmin(np.abs(t1-P[1,j]))
+        w+=np.concatenate((np.zeros(k-1),w2,np.zeros(len(w)-(k-1+len(w2)))))
     return [t1,w]
 
 def loginterp(M):
@@ -31,48 +30,61 @@ def loginterp(M):
 #Q=np.array(input())
 Spec=np.matrix([[100,20],[2000,2850],[10000,2850]])
 Spec2=loginterp(Spec)
-print(Spec2[0])
 Q=10
-hs=675
+hs=475
 P=np.zeros((4,hs))
-Fs=4*np.max(Spec[:,0])
-P[0,:]=-np.sort(-((np.max(Spec[:,0])-np.min(Spec[:,0]))*np.random.random(hs)+np.min(Spec[:,0]))) #frecuencias
-P[1,:]=np.sort(0.28*np.random.random(hs)) #diley
+tim=0.3
+Fs=10*np.max(Spec[:,0])
+ini = np.log2(np.min(Spec[:,0]))
+fin = np.log2(np.max(Spec[:,0]))
+j_typ4 = np.linspace(ini,fin,hs)
+step_typ4_ = 2**j_typ4
+#P=np.zeros((4,len(step_typ4_[np.argmin(np.abs(step_typ4_-np.min(Spec[:,0]))):])))
+P[0,:]=-np.sort(-step_typ4_)
+#hs=len(P[0,:])
+#P[0,:]=-np.sort(-((np.max(Spec[:,0])-np.min(Spec[:,0]))*np.random.random(hs)+np.min(Spec[:,0]))) #frecuencias
+P[1,:]=np.sort((tim*3/4)*np.random.random(hs)) #diley
 P[2,:]=10*np.random.random(hs) #Amp
-P[3,:]=-np.sort(-np.floor((575-3)*np.random.random(hs)+3)) #NHS
+P[3,:]=np.sort(np.floor((2*np.min(P[0,:])*tim/4-10)*np.random.random(hs)+10)) #NHS
+
 tol = 1
 req = 0
 srs_=1
 sp=40
-while tol > 0.03:
-    S=sint(P)
+dist=np.log(2)*10
+while dist > np.log(1.5):
+    S=sint(P,tim)
     #SRS
     damp_type4=1/(2*Q)
     freq_typ4=np.arange(0,Fs/2,Fs/len(S[1]))
-    k_typ4 = np.log2(Fs/2)*np.e
-    j_typ4 = np.arange(0,k_typ4)
-    step_typ4 = np.vstack(2**(j_typ4/np.e))
+    ini = np.log2(np.min(Spec[:,0]))
+    fin = np.log2(np.max(Spec[:,0]))
+    j_typ4 = np.linspace(ini,fin,hs)
+    step_typ4 = np.vstack(2**j_typ4)
     freqM=np.matlib.repmat(freq_typ4,len(j_typ4),1)
     stepM=np.matlib.repmat(step_typ4,1,len(freq_typ4))
     H=(stepM**2+1j*2*damp_type4*stepM*freqM)/(stepM**2-freqM**2+1j*2*damp_type4*stepM*freqM)
     H=np.concatenate((H,np.conj(H[:,::-1])),axis=1)
     Y3=np.matlib.repmat(np.fft.fft(S[1]),len(j_typ4),1)*H
     SRS=[step_typ4,np.max(np.abs(np.fft.ifft(Y3,axis=1)),axis=1)]
-    req=[]
-    res=[]
-    for h in np.arange(1,len(P[0,:])):
-        srs_=SRS[1][np.argmin(np.abs(SRS[0]-P[0,h]))]
-        sp=Spec2[1][np.argmin(np.abs(Spec2[0]-P[0,h]))]
-        if srs_<sp-sp/40:
-            P[2,h]=sp/srs_*P[2,h]
-        if srs_>sp+sp/40:
-            P[2,h]=sp/srs_*P[2,h]
-        res.append(srs_)
-        req.append(sp)
-    tol=npl.norm(np.array(res)-np.array(req))/npl.norm(np.array(req))
-    print(tol)
+    sp=[]
+    for h in np.arange(0, len(P[0, :])):
+        sp.append(Spec2[1][np.argmin(np.abs(Spec2[0]-P[0,h]))])
+    P[2, :] = np.array(sp)/SRS[1][::-1] * P[2, :]
+#    req=[]
+#    res=[]
+#    for h in np.arange(0,len(P[0,:])):
+#        srs_=SRS[1][np.argmin(np.abs(SRS[0]-P[0,h]))]
+#        sp=Spec2[1][np.argmin(np.abs(Spec2[0]-P[0,h]))]
+#        if np.abs(np.log(srs_)-np.log(sp))>np.log(1.0000001):
+#            P[2,h]=sp/srs_*P[2,h]
+#        res.append(srs_)
+#        req.append(sp)
+    dist=np.max(np.abs(np.array(np.log(SRS[1][::-1]))-np.array(np.log(sp))))
+    tol=npl.norm(np.array(SRS[1][::-1])-np.array(sp))/npl.norm(np.array(sp))
+    print([dist,tol])
 
-Sf=sint(P)
+Sf=sint(P,tim)
 plt.figure()
 plt.xlim([100,10000])
 plt.ylim([10,10000])
