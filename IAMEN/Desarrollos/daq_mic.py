b928b8880f8e0405d3471e99fc0ab0e1c9f8780b
hp1opticaiamend <hsosa@cnea.gov.ar>
2022-11-07 09:24:34 -0300

    modificacion en dic_mic

--- IAMEN/Desarrollos/daq_mic.py
+++ IAMEN/Desarrollos/daq_mic.py
@@ -6,8 +6,10 @@ import scipy.integrate as inte
 from matplotlib.colors import LogNorm
 
 fs=10000
-duration = 5  # seconds
-myrecording = sd.rec(duration * fs, samplerate=fs, channels=2,dtype='float64')
+duration = 10  # seconds
+#myrecording = sd.rec(duration * fs, samplerate=fs, channels=2,dtype='float64')
+#myrecording = sd.playrec(myarray, fs, channels=2)
+myrecording=np.sin(2*np.pi*(100*np.sin(2*np.pi*0.5*np.linspace(0,duration,duration*fs))+700)*np.linspace(0,duration,duration*fs))
 print("Recording Audio")
 sd.wait()
 print("Audio recording complete , Play Audio")
@@ -24,19 +26,19 @@ def cascada(x,fs,wind='hann',lin=2048,overl=50):
     return f,t,Spec
 
 
-signal=myrecording[:,0]+myrecording[:,1]
-signal_int=inte.cumtrapz(signal,dx=1/fs)
-f,t,Sxx=cascada(signal_int,fs,lin=1000,overl=50)
-plt.subplot(211)
-tiem=np.linspace(0,duration,len(myrecording[:,0])-1)
-plt.plot(tiem,signal_int)
-plt.ylabel('Voltaje [V]')
-plt.xlim([0,np.max(tiem)])
-plt.grid()
-plt.subplot(212)
-plt.pcolormesh(t,f,Sxx,shading='gouraud',norm=LogNorm(vmin=np.max(Sxx)/1E5, vmax=np.max(Sxx)))
-plt.xlabel('Tiempo [s]')
-plt.ylabel('Frecuencia [Hz]')
-plt.ylim([0,2500])
-plt.show()
+#signal=myrecording[:,0]+myrecording[:,1]
+#signal_int=inte.cumtrapz(signal,dx=1/fs)
+#f,t,Sxx=cascada(signal_int,fs,lin=1000,overl=50)
+#plt.subplot(211)
+#tiem=np.linspace(0,duration,len(myrecording[:,0])-1)
+#plt.plot(tiem,signal_int)
+#plt.ylabel('Voltaje [V]')
+#plt.xlim([0,np.max(tiem)])
+#plt.grid()
+#plt.subplot(212)
+#plt.pcolormesh(t,f,Sxx,shading='gouraud',norm=LogNorm(vmin=np.max(Sxx)/1E5, vmax=np.max(Sxx)))
+#plt.xlabel('Tiempo [s]')
+#plt.ylabel('Frecuencia [Hz]')
+#plt.ylim([0,2500])
+#plt.show()
 
